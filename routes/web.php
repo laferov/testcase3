a055cc3a59977dc7af3332ea30b57e4e7f6cec0f
Vladimir Laferov <dtreckers@gmail.com>
2023-04-28 13:42:32 +0000

    Install Laravel

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function () {
+    return 'nothing to do here';
+});
+
+Route::get('/manage', function (Request $request) {
+        echo $request->getHttpHost();
+    });
+
